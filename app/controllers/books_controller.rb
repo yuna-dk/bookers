6f52128b434e94ab932f782f75e97f7a19d08c6f
yuna-dk <149683910+yuna-dk@users.noreply.github.com>
2023-12-09 08:48:45 +0000

    [Add] assignment2

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,27 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book)
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
